8e77b1964c66fedfefef84a169d84d19a166f361
adil95400 <lamrabet.adil1@gmail.com>
2025-06-11 19:12:42 +0200

    Add product import API endpoint

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from .stripe_checkout import router as stripe_checkout_router
 from .stripe_webhook import router as stripe_webhook_router
 from .connect import router as connect_router
+from .import_extension import router as import_extension_router
 
 router = APIRouter()
 router.include_router(stripe_checkout_router)
 router.include_router(stripe_webhook_router)
 router.include_router(connect_router)
+router.include_router(import_extension_router)
